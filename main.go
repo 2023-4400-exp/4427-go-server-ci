efa762172f1ada0505353ea0ccd7e6b97385eba2
tochiman <developer@tochiman.com>
2024-01-09 13:34:05 +0900

    update message変更

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4427", "message": "ぎゃあああああああ"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4427", "message": "ぎゃあ"})
     }
 }
